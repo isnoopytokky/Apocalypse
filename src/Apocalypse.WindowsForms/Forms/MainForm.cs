905505808516c7e969d134a6385192d94ce688fe
Putta Khunchalee <ultimaweapon@outlook.com>
2016-08-17 16:50:11 +0700

    Add Apocalypse.WindowsForms project

---
+++ src/Apocalypse.WindowsForms/Forms/MainForm.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace Apocalypse.WindowsForms.Forms
+{
+    public sealed class MainForm : Form
+    {
+    }
+}
---
+++ src/Apocalypse.WindowsForms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using Apocalypse.WindowsForms.Forms;
+
+namespace Apocalypse.WindowsForms
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var mainForm = new MainForm())
+            {
+                Application.Run(mainForm);
+            }
+        }
+    }
+}
